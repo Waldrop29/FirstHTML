2d8567380e3540c239ab5a4d50a65673f966d758
Waldrop29 <spencer.a.waldrop@gmail.com>
2026-05-29 15:17:05 -0600

    updating unit list

--- groceries.js
+++ groceries.js
@@ -52,8 +52,8 @@ window.addEventListener("DOMContentLoaded", () => {
   const UNIT_LIST = [
     "packets", "cartons", "lbs", "oz", "doz", "cups",
     "tbsp", "tsp", "gallons", "liters",
-    "bags", "bottles", "jars", "cans", "boxes", "miscellaneous",
-    "Pieces", "Spoons", "Slices", "Cloves", "Heads", "Strips", "Sprigs"
+    "bags", "bottles", "jars", "cans", "boxes", "Miscellaneous",
+    "Pieces", "Spoons", "Slices", "Cloves", "Heads", "Strips", "Sprigs", ""
   ];
 
   // *load saved lists*
